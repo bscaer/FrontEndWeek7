8fddfb32365c71673c1feab9ef0a2bcd699ed0ac
Beth Scaer <bscaer@gmail.com>
2021-01-23 18:17:21 -0500

    update

---
+++ index.js
@@ -0,0 +1,16 @@
+// Create a website using only HTML that meets the following criteria.
+// Website must contain:
+//      a.	At least 3 pages
+//      b.	Links to navigate between all pages present on each page
+//      c.	A list
+//      d.	3 images
+//      e.	2 different size headers
+//      f.	A table
+//      g.	A form
+//
+// Fetal development - One webpage per trimester, an image and a description per developmental stage
+//      First trimester: 0 weeks, 8 weeks
+//      Second trimester: 16 weeks, 24 weeks
+//      Third trimester: 32 weeks, 40 weeks
+// 
+// 
